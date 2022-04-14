5f643ee6fbc386706ac4b6c9117ceae2a5c33e75
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-04-14 17:59:28 +0300

    showing current time

--- src/app.js
+++ src/app.js
@@ -1,20 +1,45 @@
 let apiKey = "197bcc774e27a469ef9bf7b4d6ee8b5e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+let now = new Date();
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#todayTemperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#weather");
-
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let currentTime = document.querySelector("#time");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML =
     response.data.weather[0].description.toUpperCase();
-
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  currentTime.innerHTML = currentDate(now);
   console.log(response.data);
+}
+function currentDate(date) {
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${days[dayIndex]} ${hours}:${minutes}`;
 }
