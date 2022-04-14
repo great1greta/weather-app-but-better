3433d28626649d045a153dcc401088e53f0e457d
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-04-14 22:38:37 +0300

    Added icons to API

--- src/app.js
+++ src/app.js
@@ -1,25 +1,8 @@
 let apiKey = "197bcc774e27a469ef9bf7b4d6ee8b5e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 let now = new Date();
 
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#todayTemperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#weather");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let currentTime = document.querySelector("#time");
-
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML =
-    response.data.weather[0].description.toUpperCase();
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  currentTime.innerHTML = currentDate(now);
-  console.log(response.data);
-}
 function currentDate(date) {
   let dayIndex = date.getDay();
   let days = [
@@ -42,4 +25,26 @@ function currentDate(date) {
     minutes = `0${minutes}`;
   }
   return `${days[dayIndex]} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#todayTemperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#weather");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let currentTime = document.querySelector("#time");
+  let iconElement = document.querySelector("#todayIcon");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML =
+    response.data.weather[0].description.toUpperCase();
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  currentTime.innerHTML = currentDate(now);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
