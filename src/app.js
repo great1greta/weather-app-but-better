1d5ac8fede269bf900e13f88eb622590fc43ed8d
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-04-21 14:12:34 +0300

    Finalised project

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-              <div class="col-2">
+              <div class="col-2" id="forecast-card">
                 <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
               
                 <img
