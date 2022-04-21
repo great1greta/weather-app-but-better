c395a7c8108e2d0d03c8569b4e65c6fffe80da11
Greta Vasiulyte <gretavasiulyte@gmail.com>
2022-04-21 09:49:23 +0300

    Fixed forecast JS

--- src/app.js
+++ src/app.js
@@ -34,8 +34,6 @@ function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastDays = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
@@ -43,7 +41,7 @@ function displayForecast(response) {
         forecastHTML +
         `
               <div class="col-2">
-                <div class="forecast-date">${forecastDay.dt}</div>
+                <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
               
                 <img
                   src="http://openweathermap.org/img/wn/${
@@ -75,7 +73,6 @@ function getForecast(coordinates) {
 
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 function displayTemperature(response) {
@@ -158,5 +155,3 @@ fahrenheitLink.addEventListener("click", showFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsius);
-
-displayForecast();
